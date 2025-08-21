b4e22a1bb8aa8f1a544ea26de372c13431677aa0
Aadharsh1006 <aadharshrameish@gmail.com>
2025-08-21 19:21:14 +0530

    Redeploy in versile.

--- src/api/blogPostService.js
+++ src/api/blogPostService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // The URL of your Spring Boot backend
-const API_BASE_URL = 'http://blogmanagementbackend-production-c050.up.railway.app/api';
+const API_BASE_URL = 'https://blogmanagementbackend-production-c050.up.railway.app/api';
 
 // Create a configured instance of Axios
 const apiClient = axios.create({
